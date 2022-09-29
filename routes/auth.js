acc38b06004e5f8520bb5f1c1490ace5fca17a9d
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-29 18:24:47 +0700

    1 file modified

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
-// const verifyToken = require('../middleware/auth')
+const verifyToken = require('../middleware/auth');
 
 const User = require('../models/User');
 
